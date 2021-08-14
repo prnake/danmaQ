60704ea4bc7ea65754ac917a10cc219bb8385e36
papersnake <prnake@gmail.com>
2021-08-14 19:31:06 +0800

    feat: add flatpak support

--- flatpak/install.sh
+++ flatpak/install.sh
@@ -1,6 +1,5 @@
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.kde.Sdk//5.15
 flatpak install flathub io.qt.qtwebengine.BaseApp//5.15
-flatpak-builder --repo=repo --force-clean build-dir org.tuna.danmaQ.json
-flatpak --user remote-add --no-gpg-verify tutorial-repo repo
-flatpak --user install tutorial-repo org.tuna.danmaQ
-flatpak runflatpak run org.tuna.danmaQ
+flatpak-builder --repo=repo --force-clean build-dir org.tuna.danmaQ.local.json
+flatpak build-bundle repo danmaQ.flatpak org.tuna.danmaQ
